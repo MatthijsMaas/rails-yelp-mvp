b436636e2706b73f158acac200153601657895e8
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-06-01 12:19:11 +0200

    added restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  "0601232151",
+    category:        3
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "2123101723",
+    category:        4
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  "624522454",
+    category:        1
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
+
